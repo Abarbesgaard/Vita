032288259325e511ebe248e480bbdff4f90b431e
Andreas B <Abarbesgaard@gmail.com>
2024-08-23 12:53:11 +0200

    Tilføjet Service

---
+++ Vita_WebAPI/Vita_WebAPI_Services/VideoService.cs
@@ -0,0 +1,27 @@
+﻿using ClassLibrary1Vita_WebAPI_Repository;
+using Vita_WebApi_Shared;
+
+namespace Vita_WebAPI_Services;
+public interface IVideoService
+{
+}
+public class VideoService(IVideoRepository repository) : IVideoService 
+{
+    private readonly IVideoRepository _repository = repository;
+
+    public async Task CreateVideo(Video video)
+    {
+        await  _repository.AddAsync(video);
+    }
+    
+    public async Task<IEnumerable<Video>> GetAllVideos()
+    {
+        return await _repository.GetAllAsync();
+    }
+    
+    public async Task<Video?> GetVideoById(int id)
+    {
+        return await _repository.GetByIdAsync(id);
+    }
+}
+
