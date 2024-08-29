91018f19d6ace0f8acf2f59f144f2952e5c235c9
Andreas B <Abarbesgaard@gmail.com>
2024-08-29 13:54:42 +0200

    VideodatabaseSetting implementation

--- Vita_WebAPI/Vita_WebAPI_Data/VideoDatabaseSetting.cs
+++ Vita_WebAPI/Vita_WebAPI_Data/VideoDatabaseSetting.cs
@@ -2,5 +2,7 @@ namespace Vita_WebAPI_Data;
 
 public class VideoDatabaseSetting
 {
-    
+   public string Host { get; set; }
+   public int Port { get; set; }
+   public string ConnectionString => $"mongodb://{Host}:{Port}";
 }
