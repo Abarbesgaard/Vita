b5d3e042c30140aff884861444f002254db6bf38
Andreas B <Abarbesgaard@gmail.com>
2024-08-29 14:10:01 +0200

    Added implementation of serive in controller

--- Vita_WebAPI/Vita_WebAPI_IdentityAPI/Controllers/ApiController.cs
+++ Vita_WebAPI/Vita_WebAPI_IdentityAPI/Controllers/ApiController.cs
@@ -1,12 +1,14 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Vita_WebAPI_Services;
 
 
 namespace Vita_WebAPI_IdentityAPI.Controllers;
 
-[Route("api")]
+[Route("users")]
 public class ApiController : Controller
 {
+   private readonly Auth0Service _auth0Service;
     [HttpGet("private")]
     [Authorize]
     public IActionResult Private()
@@ -21,6 +23,6 @@ public class ApiController : Controller
     [Authorize("read:messages")]
     public async Task Scoped()
     {
-       
+      await _auth0Service.Scoped(); 
     }
 }
