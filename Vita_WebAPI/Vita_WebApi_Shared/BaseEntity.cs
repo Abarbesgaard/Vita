a7ac18242af55846c2bdf8b05ad9af85aa111fb4
Andreas B <Abarbesgaard@gmail.com>
2024-08-29 13:53:26 +0200

    Reimplemenation of jsonpropertyname

--- Vita_WebAPI/Vita_WebApi_Shared/BaseEntity.cs
+++ Vita_WebAPI/Vita_WebApi_Shared/BaseEntity.cs
@@ -1,7 +1,10 @@
-﻿namespace Vita_WebApi_Shared;
+﻿using System.Text.Json.Serialization;
+
+namespace Vita_WebApi_Shared;
 
 public abstract class BaseEntity
 { 
+    [JsonPropertyName("id")]
     public Guid Id { get; set; }
 
 }
