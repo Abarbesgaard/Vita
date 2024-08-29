9f9a96148aaba7d030f132359b0b028aa2612957
Andreas B <Abarbesgaard@gmail.com>
2024-08-29 14:25:04 +0200

    edited auth and authen

--- Vita_WebAPI/Vita_WebAPI_IdentityAPI/Program.cs
+++ Vita_WebAPI/Vita_WebAPI_IdentityAPI/Program.cs
@@ -6,6 +6,8 @@ builder.Services.AddScoped<IAuth0Service, Auth0Service>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
+builder.Services.AddAuthentication();
+builder.Services.AddAuthorization();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -16,6 +18,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthorization();
+app.UseAuthentication();
 app.MapControllers();
 
 
