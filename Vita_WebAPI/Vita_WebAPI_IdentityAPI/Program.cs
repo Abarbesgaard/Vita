de83ecb19b4e9ba713ccb26b500d905216bd9625
Andreas B <Abarbesgaard@gmail.com>
2024-08-29 14:39:15 +0200

    Added nuget

--- Vita_WebAPI/Vita_WebAPI_IdentityAPI/Program.cs
+++ Vita_WebAPI/Vita_WebAPI_IdentityAPI/Program.cs
@@ -1,8 +1,27 @@
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 using Vita_WebAPI_Services;
 
 var builder = WebApplication.CreateBuilder(args);
-
+var auth0Settings = builder.Configuration.GetSection("Auth0");
+var domain = auth0Settings.GetValue<string>("Domain");
+var audience = auth0Settings.GetValue<string>("Audience");
 builder.Services.AddScoped<IAuth0Service, Auth0Service>();
+builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer(options =>
+    {
+        options.Authority = domain;
+        options.Audience = audience;
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            NameClaimType = ClaimTypes.NameIdentifier
+        };
+    });
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
