6d973335a83d2bdcdd1b6275b13c0fe91b2b841e
Andreas B <Abarbesgaard@gmail.com>
2024-08-29 13:52:53 +0200

    Tilføjelse til Video model

--- Vita_WebAPI/Vita_WebApi_Shared/Video.cs
+++ Vita_WebAPI/Vita_WebApi_Shared/Video.cs
@@ -1,11 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
 namespace Vita_WebApi_Shared;
 
 public class Video : BaseEntity
 {
-    
+    [JsonPropertyName("createdAt")]
     public DateTime CreatedAt { get; set; }
+    [JsonPropertyName("updatedAt")]
     public DateTime UpdatedAt { get; set; }
+    [JsonPropertyName("title")]
+    [StringLength(200, MinimumLength = 1, ErrorMessage = "Title must be between 1 and 200 characters")]
     public string Title { get; set; }
+    [JsonPropertyName("description")]
+    [StringLength(500, MinimumLength = 1, ErrorMessage = "Description must be between 1 and 500 characters")]
     public string Description { get; set; }
+    [JsonPropertyName("url")]
+    [StringLength(2083, MinimumLength = 1, ErrorMessage = "Url must be between 1 and 2083 characters")]
     public string Url { get; set; }
 }
