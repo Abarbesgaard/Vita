705d6f9da87a5230beb266c8bd7a7c9a40618b1c
Andreas B <Abarbesgaard@gmail.com>
2024-08-29 07:45:34 +0200

    Sikkerhed i entity model

--- Vita_WebAPI/Vita_WebApi_API/Dto/VideoDto.cs
+++ Vita_WebAPI/Vita_WebApi_API/Dto/VideoDto.cs
@@ -3,14 +3,14 @@ using System.ComponentModel.DataAnnotations;
 namespace Vita_WebApi_API.Dto;
 
 public record UpdateVideoDto(
-    string Title, 
-    string Description, 
-    string Url);
+    string? Title, 
+    string? Description, 
+    string? Url);
 public record VideoDto(
     Guid Id, 
     DateTimeOffset CreatedAt, 
     DateTimeOffset UpdatedAt, 
-    string Title, 
-    string Description,
-    [Required] string Url);
+    string? Title, 
+    string? Description,
+    [Required] string? Url);
     
