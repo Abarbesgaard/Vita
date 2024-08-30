07680995a4601c0b727e30840c753e5b66afed0b
Andreas B <Abarbesgaard@gmail.com>
2024-08-30 10:19:12 +0200

    Det virker

--- Vita_WebAPI/Vita_WebApi_API/Program.cs
+++ Vita_WebAPI/Vita_WebApi_API/Program.cs
@@ -2,6 +2,7 @@ using System.Reflection;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization;
@@ -41,10 +42,9 @@ public class Program
         BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
         BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
 
-        builder.Services.AddAuthentication().AddJwtBearer();
+
         builder.Services.AddAuthentication();
-        
-        
+      
         // Configure Swagger
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen(options =>
@@ -61,13 +61,25 @@ public class Program
                     Email = "Abarbesgaard@gmail.com"
                 }
             });
-            options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme() {
+             var securityScheme = new OpenApiSecurityScheme{
                 Name = "Authorization",
-                Type = SecuritySchemeType.ApiKey,
-                Scheme = "Bearer",
+                Type = SecuritySchemeType.Http,
+                Reference = new OpenApiReference
+                {
+                    Id = JwtBearerDefaults.AuthenticationScheme,
+                    Type = ReferenceType.SecurityScheme
+                }, 
+                Scheme = "bearer",
                 BearerFormat = "JWT",
                 In = ParameterLocation.Header,
-                Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 1safsfsdfdfd\"",
+                Description = "indsæt token",
+            };
+            options.AddSecurityDefinition(securityScheme.Reference.Id, securityScheme);
+            options.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    securityScheme, []
+                }
             });
             
         });
@@ -90,7 +102,6 @@ public class Program
         app.UseHttpsRedirection();
         app.UseStaticFiles();
         app.UseRouting();
-        app.UseAuthentication();
         app.UseAuthorization();
         app.MapControllers();
         app.Run();
