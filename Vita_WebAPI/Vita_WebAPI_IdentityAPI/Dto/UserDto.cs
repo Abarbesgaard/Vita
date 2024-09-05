b877adaee5640d4baee7876817268adb1c46cdd7
Andreas B <Abarbesgaard@gmail.com>
2024-09-05 11:55:30 +0200

    Removed Warnings

--- Vita_WebAPI/Vita_WebAPI_IdentityAPI/Dto/UserDto.cs
+++ Vita_WebAPI/Vita_WebAPI_IdentityAPI/Dto/UserDto.cs
@@ -1,9 +1,9 @@
-namespace Vita_WebApi_API.Dto;
+namespace Vita_WebAPI_IdentityAPI.Dto;
 
 public class UserDto
 { 
-    public string Id { get; set; }
+    public string? Id { get; set; }
     public string? UserName { get; set; }
     public string? Email { get; set; }
-    public IList<string> Roles { get; set; }
+    public IList<string>? Roles { get; set; }
 }
