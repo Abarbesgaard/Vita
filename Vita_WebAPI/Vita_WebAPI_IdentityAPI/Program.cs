4bb7a95c27362372d6da1117f7caa0a2bd3a2a83
Andreas B <Abarbesgaard@gmail.com>
2024-09-05 11:46:19 +0200

    Cleanup + tests

--- Vita_WebAPI/Vita_WebAPI_IdentityAPI/Program.cs
+++ Vita_WebAPI/Vita_WebAPI_IdentityAPI/Program.cs
@@ -2,6 +2,7 @@ using System.Security.Claims;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.IdentityModel.Tokens;
+using Vita_WebAPI_IdentityAPI.Helpers;
 using Vita_WebAPI_Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -51,45 +52,6 @@ app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
 
-
-app.UseEndpoints(endpoints =>
-{
-    endpoints.MapControllers();
-});
-
+app.MapControllers();
 
 app.Run();
-// HasScopeRequirement.cs
-
-public class HasScopeRequirement : IAuthorizationRequirement
-{
-    public string Issuer { get; }
-    public string Scope { get; }
-
-    public HasScopeRequirement(string scope, string issuer)
-    {
-        Scope = scope ?? throw new ArgumentNullException(nameof(scope));
-        Issuer = issuer ?? throw new ArgumentNullException(nameof(issuer));
-    }
-}
-
-// HasScopeHandler.cs
-
-public class HasScopeHandler : AuthorizationHandler<HasScopeRequirement>
-{
-    protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, HasScopeRequirement requirement)
-    {
-        // If user does not have the scope claim, get out of here
-        if (!context.User.HasClaim(c => c.Type == "scope" && c.Issuer == requirement.Issuer))
-            return Task.CompletedTask;
-
-        // Split the scopes string into an array
-        var scopes = context.User.FindFirst(c => c.Type == "scope" && c.Issuer == requirement.Issuer).Value.Split(' ');
-
-        // Succeed if the scope array contains the required scope
-        if (scopes.Any(s => s == requirement.Scope))
-            context.Succeed(requirement);
-
-        return Task.CompletedTask;
-    }
-}
