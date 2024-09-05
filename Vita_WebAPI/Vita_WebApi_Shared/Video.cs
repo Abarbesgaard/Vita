c80a541d6e4a85be42a0d8e6ce34988610d792a1
Andreas B <Abarbesgaard@gmail.com>
2024-09-05 11:54:59 +0200

    Removed warnings

--- Vita_WebAPI/Vita_WebApi_Shared/Video.cs
+++ Vita_WebAPI/Vita_WebApi_Shared/Video.cs
@@ -30,5 +30,5 @@ public class Video : BaseEntity
     
     [JsonPropertyName("url")]
     [StringLength(2083, MinimumLength = 1, ErrorMessage = "Url must be between 1 and 2083 characters")]
-    public required string Url { get; set; }
+    public required string? Url { get; set; }
 }
