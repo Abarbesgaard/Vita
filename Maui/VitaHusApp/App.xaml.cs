dfcc5e67b59ccbb023c209e5ae42004a3e5c55b4
Cæcilie Klinkwort <Cklinkwort@gmail.com>
2024-09-10 11:44:39 +0200

    Maui

---
+++ Maui/VitaHusApp/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using VitaHusApp.Pages;
+
+namespace VitaHusApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new VideoMenu();
+        }
+    }
+}
---
+++ Maui/VitaHusApp/Model/Root.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace VitaHusApp.Model
+{
+    public  class Root
+    {
+        public string Id { get; set; }
+        public string? UpdatedBy { get; set; }
+
+        public string? CreatedBy { get; set; }
+
+     
+        public DateTimeOffset CreatedAt { get; init; }
+
+        public DateTimeOffset UpdatedAt { get; init; }
+
+        
+        public string? Title { get; set; }
+
+        public string? Description { get; set; }
+
+      
+    }
+}
