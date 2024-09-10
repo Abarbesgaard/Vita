9f8a8231eca1367e8dbd14e33bc683c6f61fcd7c
Cæcilie Klinkwort <Cklinkwort@gmail.com>
2024-09-10 11:44:39 +0200

    Maui

---
+++ Maui/VitaHusApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace VitaHusApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Maui/VitaHusApp/Services/VideoService.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
+using VitaHusApp.Model;
+
+namespace VitaHusApp.Services
+{
+    public static class VideoService
+    {
+      
+        public static async Task<Root> GetVideo(string Id, string title)
+        {
+            var httpClient = new HttpClient();
+             var response = await httpClient.GetStringAsync(""); //her skal url ind
+            return JsonConvert.DeserializeObject<Root>(response);
+        }
+        public static async Task<Root> GetAllVideo(string Id)
+        {
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetStringAsync(""); //her skal url ind
+            return JsonConvert.DeserializeObject<Root>(response);
+        }
+    }
+}
