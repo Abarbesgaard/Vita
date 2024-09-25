f4f35eaddd524114fd7a3fba642e3707c53b4676
Andreas B <Abarbesgaard@gmail.com>
2024-09-25 12:36:19 +0200

    adding in generic  and removing type services

--- Vita_WebAPI/Vita_WebAPI_Services/IActivityService.cs
+++
@@ -1,9 +0,0 @@
-using Vita_WebApi_Shared;
-namespace Vita_WebAPI_Services;
-
-
-public interface IActivityService
-{
-    Task<IEnumerable<Activity>> GetAll();
-  
-}
--- Vita_WebAPI/Vita_WebAPI_Services/IAuditLogService.cs
+++ Vita_WebAPI/Vita_WebAPI_Services/IAuditLogService.cs
@@ -7,14 +7,16 @@ public interface IAuditLogService
    Task LogAsync(AuditLog auditLog);
 }
 
+public abstract class AuditData
+{
+    public Guid? Id { get; set; }
+}
 public class AuditLog
 {
-      public string? UserId { get; set; }
+      public Guid UserId { get; set; }
       public string? Operation { get; set; }
       public string? Collection { get; set; }
-      public Guid DocumentId { get; set; }
-      public object? Before { get; set; }
-      public object? After { get; set; }
+      public Guid? DocumentId { get; set; }
       public DateTimeOffset Timestamp { get; set; }
       
 }
