0bbffdb3b055698b9c8c86d3446617c7c402cd0f
Andreas B <Abarbesgaard@gmail.com>
2024-09-27 09:26:32 +0200

    Opdateret Video Model

--- Vita_WebAPI/Vita_WebApi_Shared/Video.cs
+++ Vita_WebAPI/Vita_WebApi_Shared/Video.cs
@@ -10,4 +10,13 @@ public class Video : BaseEntity
     [JsonPropertyName("url")]
     [StringLength(2083, MinimumLength = 1, ErrorMessage = "Url must be between 1 and 2083 characters")]
     public required string? Url { get; set; }
+
+    [JsonPropertyName("Visible")] 
+    public bool Visible { get; set; } = false;
+    
+    [JsonPropertyName("GroupId")]
+    public Guid? GroupId { get; set; }
+    
+    [JsonPropertyName("GroupNumber")]
+    public int? GroupNumber { get; set; }
 }
